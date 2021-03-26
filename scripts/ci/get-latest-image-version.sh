240af922980a869ee581d9389a337dc650ca6304
Ben Vickers <bvickers@vmware.com>
2021-03-26 18:27:01 +0100

    fix grep in get-img-version

--- scripts/ci/get-latest-image-version.sh
+++ scripts/ci/get-latest-image-version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-IMG="$(kp image status angular-demo | grep | awk '{print $2}')"
+IMG="$(kp image status angular-demo | grep "LatestImage" | awk '{print $2}')"
 echo "$IMG"
 echo "$IMG" > img-version
